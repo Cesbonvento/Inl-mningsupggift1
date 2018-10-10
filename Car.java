706804b7cb2bda02f58851075eb57434f1b59a93
Cesbonvento <43865799+Cesbonvento@users.noreply.github.com>
2018-10-10 19:14:37 +0200

    Add files via upload

---
+++ Car.java
@@ -0,0 +1,28 @@
+/*
+ * Javautveckling 2018
+ */
+package practice1_a;
+
+/**
+ *
+ * @author fidel.diaz
+ */
+public class Car { 
+    protected String regNr; 
+    protected String model; 
+    protected String brand; 
+//    CarOwner carOwner; 
+    
+    public Car(String regNr, String model, String brand) 
+    {    
+        this.regNr = regNr; 
+        this.model = model; 
+        this.brand = brand; 
+    }
+//    public void setCar(CarOwner carOwner){
+//        this.carOwner = carOwner; 
+//    }
+
+    
+    
+}
